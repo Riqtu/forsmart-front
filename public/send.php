1af3e68e9bb057068571ef7c992faee6f2f5b153
Riqtu <iriqtu@gmail.com>
2020-02-27 12:52:09 +0300

    Add mailer

--- public/send.php
+++ public/send.php
@@ -1,11 +1,51 @@
-<?php
-    $fio = $_POST['fio'];
-    $email = $_POST['email'];
-    $fio = htmlspecialchars($fio);
-    $email = htmlspecialchars($email);
-    $fio = urldecode($fio);
-    $email = urldecode($email);
-    $fio = trim($fio);
-    $email = trim($email);
-    mail("xxx-zet@mail.ru", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: forsmart@mail.ru \r\n");
+<?php 
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+header('Content-Type: application/json');
+// $fio = $_POST['fio'];
+// $email = $_POST['email'];
+// $course = $_POST['course'];
+// $fio = htmlspecialchars($fio);
+// $email = htmlspecialchars($email);
+// $course = htmlspecialchars($course);
+// $fio = urldecode($fio);
+// $email = urldecode($email);
+// $course = urldecode($course);
+// $fio = trim($fio);
+// $email = trim($email);
+// $course = trim($course);
+if (array_key_exists('name', $_POST)) {
+    require './PHPMailer.php';
+    require './SMTP.php';
+    
+    $mail = new PHPMailer;
+    $mail->CharSet = 'UTF-8';
+    
+    // Настройки SMTP
+    $mail->isSMTP();
+    $mail->SMTPAuth = true;
+    $mail->SMTPDebug = 0;
+    
+    $mail->Host = 'ssl://smtp.mail.ru';
+    $mail->Port = 465;
+    $mail->Username = 'xxx-zet@mail.ru';
+    $mail->Password = 'zerogravity1';
+    
+    // От кого
+    $mail->setFrom('xxx-zet@mail.ru', 'Forsmart agency');		
+    
+    // Кому
+    $mail->addAddress('xxx-zet@mail.ru', '');
+    
+    // Тема письма
+    $mail->Subject = 'Новая заявка!';
+    
+    // Тело письма
+    $body = "Имя: " .$_POST["name"].'<br>Телефон: '.$_POST['phone'].'<br>Курс: '.$_POST['course'];
+    $mail->msgHTML($body);
+    $mail->send();
+    echo json_encode($_POST);
+}
+
 ?>
